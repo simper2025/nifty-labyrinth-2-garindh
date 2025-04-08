891b823938b1ce02e99c391c02876712fcd890d9
Garin Humpherys <garin.humpherys@students.snow.edu>
2025-04-08 16:35:04 -0600

    finished the easy maze

--- Main.cpp
+++ Main.cpp
@@ -9,10 +9,10 @@
  * do NOT edit the value of kYourName. Changing kYourName will change which
  * maze you get back, which might invalidate all your hard work!
  */
-const std::string kYourName = "TODO: Replace this string with your name.";
+const std::string kYourName = "Garin Humpherys";
 
 /* Change these constants to contain the paths out of your mazes. */
-const std::string kPathOutOfNormalMaze = "TODO: Replace this string with your path out of the normal maze.";
+const std::string kPathOutOfNormalMaze = "ESEENSWSWWNSESEE";
 const std::string kPathOutOfTwistyMaze = "TODO: Replace this string with your path out of the twisty maze.";
 
 int main() {
